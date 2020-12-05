c6ce3ac5f39115d0c88d751872644aab0944b3e9
paulcaruana <caruanapaul023@gmail.com>
2020-12-05 15:18:03 +1100

    Tidyup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,14 @@ import { NextPage } from 'next'
 import styled from 'styled-components'
 import AppHead from '~/components/AppHead'
 import StyledBox from '~/components/Box'
-
+import { Button } from '~/components/Button'
 const IndexInner = styled.div``
 
 const Index: NextPage = () => (
   <IndexInner>
     <AppHead title="Homepage" />
     <StyledBox>Hi! Im a bx!</StyledBox>
+    <Button href={'/'}>Test</Button>
   </IndexInner>
 )
 
